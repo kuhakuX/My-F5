33d63a8dd332a2361aa60a2ac858c442c0f13157
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-03-25 03:01:48 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,3 +4,6 @@ while keyboard.is_pressed('x')==False:
     pyautogui.click(x=1000, y=900)
     pyautogui.click(x=1000, y=995)
     pyautogui.click(x=1000, y=770)
+    pyautogui.click(x=1350, y=980)
+    pyautogui.click(x=1000, y=700)
+    pyautogui.click(x=1000, y=600)
